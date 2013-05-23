acfcb4474791707f01befe5ed0f3d52feb5e937f
kaivalya shah <kshah@ajooba.ca>
2013-05-22 20:25:22 -0400

    myhome menu

---
+++ resources/js/lib/myapps.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+// Drop down for profile setting
+
+			function DropDown(el) {
+				this.dd = el;
+				this.initEvents();
+			}
+			DropDown.prototype = {
+				initEvents : function() {
+					var obj = this;
+
+					obj.dd.on('click', function(event){
+						$(this).toggleClass('active');
+						event.stopPropagation();
+					});	
+				}
+			}
+
+			$(function() {
+
+				var dd = new DropDown( $('#dd') );
+
+				$(document).click(function() {
+					// all dropdowns
+					$('.wrapper-dropdown-5').removeClass('active');
+				});
+
+			});
